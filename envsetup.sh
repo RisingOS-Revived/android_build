a070205cebc2034e2d2e448f7825f791de932d43
Alvin Francis <nivlafx@gmail.com>
2025-01-11 02:35:45 -0400

    envsetup: Update to new path guidelines

--- envsetup.sh
+++ envsetup.sh
@@ -1628,8 +1628,8 @@ function sign_build() {
     fi
     echo "Creating RisingOS JSON OTA..."
     $ANDROID_BUILD_TOP/vendor/rising/build/tools/createjson.sh "$target_device" "$OUT" "RisingOS-$rising_build_version-ota-signed.zip" "$rising_version" "$rising_codename" "$rising_package_type" "$rising_release_type"
-    local json_file="${rising_package_type}_${target_device}.json"
-    cp -f "$OUT/$json_file" "vendor/risingOTA/$json_file"
+    local json_file="${target_device}.json"
+    cp -f "$OUT/$json_file" "vendor/official_devices/OTA/device/${rising_package_type}/$json_file"
     echo "RisingOS JSON OTA created and copied."
 }
 
