00cbca9020d9948501caa9e545e365b2686c3ae8
Arman-ATI <altafiarman5@gmail.com>
2025-08-13 12:12:08 +0000

    Add a function to apply gms fixups

--- envsetup.sh
+++ envsetup.sh
@@ -1252,6 +1252,32 @@ unset syswrite
 unset tomlgrep
 unset treegrep
 
+function apply_gms_fixes() {
+    local files=(
+        "$ANDROID_BUILD_TOP/vendor/gms/system_ext/packages/privileged_apps/NexusLauncherRelease/Android.mk"
+        "$ANDROID_BUILD_TOP/vendor/gms/system_ext/packages/privileged_apps/AvatarPickerGoogle/Android.mk"
+    )
+
+    for android_mk_file in "${files[@]}"; do
+        if [ -f "$android_mk_file" ]; then
+            local basename_file
+            basename_file=$(basename "$(dirname "$android_mk_file")")
+            if grep -q "^LOCAL_OVERRIDES_PACKAGES" "$android_mk_file"; then
+                echo "Removing LOCAL_OVERRIDES_PACKAGES from ${basename_file}/Android.mk..."
+                sed -i '/^LOCAL_OVERRIDES_PACKAGES/d' "$android_mk_file"
+                echo "Done."
+            fi
+        fi
+    done
+
+    local settings_mk="$ANDROID_BUILD_TOP/vendor/gms/product/packages/privileged_apps/SettingsIntelligenceGooglePrebuilt/Android.mk"
+    if [ -f "$settings_mk" ] && grep -q "LOCAL_CERTIFICATE := PRESIGNED" "$settings_mk"; then
+        echo "Updating certificate in SettingsIntelligenceGooglePrebuilt/Android.mk..."
+        sed -i 's/LOCAL_CERTIFICATE := PRESIGNED/LOCAL_UPDATABLE := false\nLOCAL_CERTIFICATE := platform/' "$settings_mk"
+        echo "Done."
+    fi
+}
+
 function setup_ccache() {
     if [ -z "${CCACHE_EXEC}" ]; then
         if command -v ccache &>/dev/null; then
@@ -2164,6 +2190,8 @@ set_global_paths
 source_vendorsetup
 addcompletions
 setup_ccache
+
+apply_gms_fixes
 
 if [[ "$USE_LEFTOVERS" -eq 1 ]]; then
   leftovers
